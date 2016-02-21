66a99754482706d78826d84a155c8b93b40fa4cf
ogzkhrmn <ogz.khrmn52@gmail.com>
2016-02-21 16:15:07 +0200

    --deneme

--- Daily-Activity-Monitoring/Daily-Activity-Monitoring/ekranayazdir.cpp
+++ Daily-Activity-Monitoring/Daily-Activity-Monitoring/ekranayazdir.cpp
@@ -2,15 +2,18 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 using namespace std;
-
+using namespace cv;
 ekranayazdir::ekranayazdir(int sayi)
 {
 	hour =sayi;
 }
 
 void ekranayazdir::yazdir() {
+	cout << "OpenCV Version: " << CV_VERSION << endl;
 	cout << hour;
 }
 
